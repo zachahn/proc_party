94717993e25684a8b4a2c77f49436749fa6b45f7
Zach Ahn <engineering@zachahn.com>
2017-04-15 01:11:35 -0400

    Add ProcParty#to_proc
    This is the basic prerequisite functionality

--- lib/proc_party.rb
+++ lib/proc_party.rb
@@ -1,5 +1,7 @@
 require "proc_party/version"
 
 module ProcParty
-  # Your code goes here...
+  def to_proc
+    method(:call).to_proc
+  end
 end
--- test/proc_party_test.rb
+++ test/proc_party_test.rb
@@ -1,8 +1,16 @@
 require "test_helper"
 
 class ProcPartyTest < TestCase
-  def test_that_it_has_a_version_number
-    refute_nil ::ProcParty::VERSION
+  class Triple
+    include ProcParty
+
+    def call(n)
+      n * 3
+    end
+  end
+
+  def test_can_be_used_as_block
+    assert_equal([3, 6, 9], (1..3).map(&Triple.new))
   end
 
   def test_it_does_something_useful
